3348afad8ba2c7ff19627262db24a43db1f7ac35
Collin <cwaquick@gmail.com>
2025-05-13 14:35:48 -0500

    Restore firebase config and test with page making API calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
-// import { Pokemon } from "@/models/pokemon";
-// import { firebaseService } from "@/services/firebase-service";
-// import PokedexTile from "./pokedex/pokedex-tile";
+import { Pokemon } from "@/models/pokemon";
+import { firebaseService } from "@/services/firebase-service";
+import PokedexTile from "./pokedex/pokedex-tile";
 
 export default async function Home() {
-  // let pokemonListHtml: React.ReactNode[] = [];
-  // await firebaseService.getPokemon().then((pokemonList) => {
-  //   pokemonListHtml = pokemonList.map((pokemon: Pokemon, idx: number) => (
-  //     <PokedexTile key={idx} pokemon={pokemon} />
-  //   ));
-  // });
+  let pokemonListHtml: React.ReactNode[] = [];
+  await firebaseService.getPokemon().then((pokemonList) => {
+    pokemonListHtml = pokemonList.map((pokemon: Pokemon, idx: number) => (
+      <PokedexTile key={idx} pokemon={pokemon} />
+    ));
+  });
 
   return (
     <>
     <div className="centered">
-      <h1>TESTING PREVIEW IN PR</h1>
+      <h1>Pokemon Amethyst Helper</h1>
+      {/* TODO Later: this should be it's own component */}
+      <div className="pokedex-container">
+        {pokemonListHtml}
+      </div>
+      <p>Testing with deployment and firebase</p>
     </div>
     </>
   );
