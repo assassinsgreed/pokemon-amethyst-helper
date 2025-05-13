b1d6ae7b79fa4e6336911d9ba361102f2f5bea0e
Collin <cwaquick@gmail.com>
2025-05-13 13:43:21 -0500

    Update coloir var name

--- src/app/pokedex/pokedex-tile.tsx
+++ src/app/pokedex/pokedex-tile.tsx
@@ -4,13 +4,13 @@ import "./pokedex-tile.css";
 
 export default function PokedexTile({ pokemon }: { pokemon: Pokemon }) {
     const iconUrl = `https://projectpokemon.org/images/sprites-models/sv-sprites-home/${pokemon.national_pokedex_number.toString().padStart(4, "0")}.png`;
-    const type1CSS = `var(--type_${pokemon.type[0]})`;
-    const type2CSS = pokemon.type[1] ? `var(--type_${pokemon.type[1]})` : type1CSS;
+    const firstTypeColourVar = `var(--type_${pokemon.type[0]})`;
+    const secondTypeColourVar = pokemon.type[1] ? `var(--type_${pokemon.type[1]})` : firstTypeColourVar;
 
     return (
         <div
             className="pokedex-tile"            
-            style={{ backgroundImage: `linear-gradient(to bottom right, ${type1CSS}, ${type2CSS})` }}
+            style={{ backgroundImage: `linear-gradient(to bottom right, ${firstTypeColourVar}, ${secondTypeColourVar})` }}
         >
             <Image src={iconUrl} alt={pokemon.name} className="pokedex-tile__icon" />
             <b>{pokemon.name}</b>
