140edf5d1a90c0c42205927cdf16192ac7b4c97d
assassinsgreed <assassinsgreed@users.noreply.github.com>
2025-05-13 19:31:49 +0000

    Deploy preview for PR 2 🛫

--- pr-preview/pr-2/models/pokemon.ts
+++
@@ -1,8 +0,0 @@
-export interface Pokemon {
-    national_pokedex_number: number;
-    kulure_pokedex_number: number;
-    name: string;
-    type: string[];
-    abilities: string[];
-    base_stats: number[]; // [hp, attack, defense, specialAttack, specialDefense, speed]
-}
