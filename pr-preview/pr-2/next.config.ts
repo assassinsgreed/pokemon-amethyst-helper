0f390df4816c088f00fd128b499192828b0e79a8
assassinsgreed <assassinsgreed@users.noreply.github.com>
2025-05-13 21:22:21 +0000

    Deploy preview for PR 2 🛫

---
+++ pr-preview/pr-2/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const prBasePath = process.env.NEXT_PUBLIC_PR_BASE_PATH || "";
+
+const nextConfig: NextConfig = {
+  basePath: prBasePath,
+  assetPrefix: prBasePath,
+  // images: {
+  //   unoptimized: true,
+  // },
+};
+
+export default nextConfig;
---
+++ pr-preview/pr-2/src/app/pokedex/pokedex-tile.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Pokemon } from "@/models/pokemon";
+import "./pokedex-tile.css";
+
+export default function PokedexTile({ pokemon }: { pokemon: Pokemon }) {
+    const iconUrl = `https://projectpokemon.org/images/sprites-models/sv-sprites-home/${pokemon.national_pokedex_number.toString().padStart(4, "0")}.png`;
+    const firstTypeColourVar = `var(--type_${pokemon.type[0]})`;
+    const secondTypeColourVar = pokemon.type[1] ? `var(--type_${pokemon.type[1]})` : firstTypeColourVar;
+
+    return (
+        <div
+            className="pokedex-tile"            
+            style={{ backgroundImage: `linear-gradient(to bottom right, ${firstTypeColourVar}, ${secondTypeColourVar})` }}
+        >
+            <Image
+                src={iconUrl}
+                alt={pokemon.name}
+                width={48}
+                height={48}
+                className="pokedex-tile__icon" />
+            <b>{pokemon.name}</b>
+        </div>
+    );   
+}
