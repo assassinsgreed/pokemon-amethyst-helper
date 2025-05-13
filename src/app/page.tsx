ece8443b47e4c1fbf8a7a182a600f2e728c805be
Collin <cwaquick@gmail.com>
2025-05-13 13:55:07 -0500

    Fix lint for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { firebaseService } from "@/services/firebase-service";
 import PokedexTile from "./pokedex/pokedex-tile";
 
 export default async function Home() {
-  let pokemonListHtml: React.ReactNode[] = [];
-  await firebaseService.getPokemon().then((pokemonList) => {
-    pokemonListHtml = pokemonList.map((pokemon: Pokemon, idx: number) => (
-      <PokedexTile key={idx} pokemon={pokemon} />
-    ));
-  });
+  // let pokemonListHtml: React.ReactNode[] = [];
+  // await firebaseService.getPokemon().then((pokemonList) => {
+  //   pokemonListHtml = pokemonList.map((pokemon: Pokemon, idx: number) => (
+  //     <PokedexTile key={idx} pokemon={pokemon} />
+  //   ));
+  // });
 
   return (
     <>
