801f1f5114550175057f4190154ec02345bd143a
Collin <cwaquick@gmail.com>
2025-05-13 16:16:39 -0500

    Trying default config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const prBasePath = process.env.NEXT_PUBLIC_PR_BASE_PATH || "";
+// const prBasePath = process.env.NEXT_PUBLIC_PR_BASE_PATH || "";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: prBasePath,
-  assetPrefix: prBasePath,
-  images: {
-    unoptimized: true,
-  },
-};
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   basePath: prBasePath,
+//   assetPrefix: prBasePath,
+//   images: {
+//     unoptimized: true,
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
