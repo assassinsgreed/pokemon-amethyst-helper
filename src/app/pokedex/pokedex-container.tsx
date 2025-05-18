a761dbac244d903dfc0288e98af320eb36e6c626
Collin <cwaquick@gmail.com>
2025-05-18 08:28:32 -0500

    Restore global styles and arrange color vars alphabetically

--- src/app/pokedex/pokedex-container.tsx
+++ src/app/pokedex/pokedex-container.tsx
@@ -29,7 +29,7 @@ async function PokedexContainer() {
     return (
         <>
             <div className="centered">
-                <h1 className="text-3xl font-bold">Pokedex</h1>
+                <h1>Pokedex</h1>
                 <p className="m-8">Click on a Pokemon to view its details.</p>
             </div>
             <div className="pokedex-container">
