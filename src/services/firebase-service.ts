86b0080738917304d9a4c1433ef6650496a49578
Collin <cwaquick@gmail.com>
2025-05-17 19:24:29 -0500

    Revert "Troubleshooting configuration"
    This reverts commit db811a09aa73959b2192a2c0654fe435f0bcb84c.

--- src/services/firebase-service.ts
+++ src/services/firebase-service.ts
@@ -8,10 +8,6 @@ class FirebaseService {
     constructor() {
         let app: App;
         if (getApps().length === 0) {
-            if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
-                throw new Error("Missing Firebase Admin credentials in environment variables.");
-            }
-
             app = initializeApp({
                 credential: cert({
                     projectId: process.env.FIREBASE_PROJECT_ID,
@@ -32,14 +28,9 @@ class FirebaseService {
 
     public async getPokemon(): Promise<Pokemon[]> {
         const db = this.getFirestoreInstance();
-        try {
-            const pokemonCollection = await db.collection("pokemon").get();
-            const pokemonList: Pokemon[] = pokemonCollection.docs.map(doc => doc.data() as Pokemon);
-            return pokemonList;
-        } catch (err) {
-            console.error("Error fetching Pokemon data:", err);
-            throw err;
-        }
+        const pokemonCollection = await db.collection("pokemon").get();
+        const pokemonList: Pokemon[] = pokemonCollection.docs.map(doc => doc.data() as Pokemon);
+        return pokemonList;
     }
 }
 
