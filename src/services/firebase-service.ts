20dbe684ef5cff3bfc81908296b4b8acaa2cc08b
Collin <cwaquick@gmail.com>
2025-05-17 19:00:41 -0500

    switc to sever-side API calls

--- src/services/firebase-service.ts
+++ src/services/firebase-service.ts
@@ -1,22 +1,24 @@
 import { Pokemon } from "@/models/pokemon";
-import { initializeApp } from "firebase/app";
-import { getFirestore, Firestore, collection, getDocs } from "firebase/firestore";
+import { initializeApp, cert, getApps, App } from "firebase-admin/app";
+import { getFirestore, Firestore } from "firebase-admin/firestore";
 
 class FirebaseService {
     private firestore: Firestore;
 
     constructor() {
-        const firebaseConfig = {
-            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-            authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-            measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-        };
-
-        const app = initializeApp(firebaseConfig);
+        let app = App;
+        if (getApps().length === 0) {
+            app = initializeApp({
+                credential: cert({
+                    projectId: process.env.FIREBASE_PROJECT_ID,
+                    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+                }),
+            });
+        } else {
+            app = getApps()[0];
+        }
+        
         this.firestore = getFirestore(app);
     }
 
@@ -26,7 +28,7 @@ class FirebaseService {
 
     public async getPokemon(): Promise<Pokemon[]> {
         const db = this.getFirestoreInstance();
-        const pokemonCollection = await getDocs(collection(db, "pokemon"));
+        const pokemonCollection = await db.collection("pokemon").get();
         const pokemonList: Pokemon[] = pokemonCollection.docs.map(doc => doc.data() as Pokemon);
         return pokemonList;
     }
