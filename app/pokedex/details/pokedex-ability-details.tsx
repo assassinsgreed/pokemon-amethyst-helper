34ce1b5aaf71f6d08f38e18da20ec25e8f990365
Collin <cwaquick@gmail.com>
2025-06-20 08:34:06 -0500

    Clarification for ability skipping

--- app/pokedex/details/pokedex-ability-details.tsx
+++ app/pokedex/details/pokedex-ability-details.tsx
@@ -7,7 +7,7 @@ export default function PokedexAbilityDetails({ abilities }: { abilities: string
             <h3 className="mr-1.5">Abilities:</h3>
             {abilities
                 .map((ability, i) => {
-                    if (!ability) return null;
+                    if (!ability) return null; // Some Pokemon do not have their second normal ability or hidden ability, so we skip those
                     return (
                         <span key={ability}>
                             {i === 1 && <span className="mx-1.5">/</span>}
